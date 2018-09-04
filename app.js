5e5ec415f997eda7d44fb093c7cb734b19f96537
shauryak3 <shauryakalia@gmail.com>
2018-09-04 15:27:01 +0530

    grab latitude longitutde

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const request = require('request');
 
 request({
-    url : 'https://maps.googleapis.com/maps/apigeocode/json?address=Sector%2010A%20Gurgaon%20Haryana',
+    url : 'https://maps.googleapis.com/maps/apigeocode/json?address=lombard%20street%20philadelphia',
     json : true
 },(error , response , body) => {
-    console.log(body);
+    console.log(`Address : ${body.results[0].formatted_address}`);
+    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);    
 });
