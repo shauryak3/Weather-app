3f5a6891406a1042a7d59b52bc83684a719304f2
shauryak3 <shauryakalia@gmail.com>
2018-09-04 16:21:57 +0530

    encode input address from user

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 //NPM modules
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a : {
+            demand : true,
+            alias : 'address',
+            describe : 'give your address to fetch weatherfor',
+            string : true
+        }
+    })
+    .help()
+    .argv;
+
+let encodedAddress = decodeURIComponent('argv.address');
 
 request({
-    url : 'https://maps.googleapis.com/maps/apigeocode/json?address=lombard%20street%20philadelphia',
+    url : `https://maps.googleapis.com/maps/apigeocode/json?address=${encodedAddress}`,
     json : true
 },(error , response , body) => {
     console.log(`Address : ${body.results[0].formatted_address}`);
