cead7f8a9e84e373510b8e9549bb6b930a3e21fc
shauryak3 <shauryakalia@gmail.com>
2018-09-05 13:06:21 +0530

    Error Object handled

--- app.js
+++ app.js
@@ -14,13 +14,20 @@ const argv = yargs
     .help()
     .argv;
 
-let encodedAddress = decodeURIComponent('argv.address');
+var encodedAddress = decodeURIComponent('argv.address');
 
 request({
-    url : `https://maps.googleapis.com/maps/apigeocode/json?address=${encodedAddress}`,
+    url : `https://cloud.google.com/maps-platform/maps/apigeocode/json?address=${encodedAddress}`,
     json : true
 },(error , response , body) => {
-    console.log(`Address : ${body.results[0].formatted_address}`);
-    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);    
+    if(error) {
+        console.log('unable to connect to google severs');
+    }
+    else if(body.status === 'ZERO_RESULTS'){
+        console.log('unable to find the address');
+    }else if(body.status === 'OK'){
+        console.log(`Address : ${body.results[0].formatted_address}`);
+        console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    }    
 });
