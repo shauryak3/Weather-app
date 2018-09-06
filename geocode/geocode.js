eba0f0b676d45b8cbc04633a3058a58ac2668cf4
shauryak3 <shauryakalia@gmail.com>
2018-09-06 12:39:26 +0530

    abstracting : cleaning module for geocode

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -9,14 +9,17 @@ var geocodeAddress = (address) => {
         json : true
     },(error , response , body) => {
         if(error) {
-            console.log('unable to connect to google severs');
+            callback('Unable to connect to google servers');
         }
         else if(body.status === 'ZERO_RESULTS'){
-            console.log('unable to find the address');
-        }else if(body.status === 'OK'){
-            console.log(`Address : ${body.results[0].formatted_address}`);
-            console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+            callback('unable to find the address');
+        }
+        else if(body.status === 'OK'){
+            callback(undefined,{
+                address : body.results[0].formatted_address,
+                lat : body.results[0].geometry.location.lat,
+                long : body.results[0].geometry.location.lng
+            });
         }    
     });
 };
