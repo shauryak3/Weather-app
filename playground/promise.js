9e8798327a1d3091a416f6a606caf821b261ab15
shauryak3 <shauryakalia@gmail.com>
2018-09-10 20:10:34 +0530

    playground : promise call from function

--- playground/promise.js
+++ playground/promise.js
@@ -1,15 +1,33 @@
-var somePromise = new Promise((resolve , reject) => {
-    //asynchronous work
-    setTimeout(() => {
+var asyncAdd = (a , b) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if( typeof a === 'number' && typeof b === 'number') {
+                resolve(a+b);
+            }else {
+                reject('please input numbers only');
+            }
+        }, 2500);
+    });
+};
 
-    }, 5000); 
-    // resolve('It worked');
-    reject('promise failed');
-});
-
-somePromise.then((msg) => {
-    // if resolved
-    console.log(msg);
-},(errorMsg) => {
+asyncAdd(5,7).then((result) => {
+    console.log('result is', result);
+    },(errorMsg) => {
     console.log('error :',errorMsg );
 });
+
+// var somePromise = new Promise((resolve , reject) => {
+//     //asynchronous work
+//     setTimeout(() => {
+
+//     }, 5000); 
+//     // resolve('It worked');
+//     reject('promise failed');
+// });
+
+// somePromise.then((msg) => {
+//     // if resolved
+//     console.log(msg);
+// },(errorMsg) => {
+//     console.log('error :',errorMsg );
+// });
